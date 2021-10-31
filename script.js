edd235e89b17bdcf53a332a796a82189b2a9e0d1
Lucy Lee <gamesandhannah@gmail.com>
2021-10-31 01:26:21 -0400

    Create function displayTime that prints current time every second

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+// Print out the current time to index.html
+function displayTime()
+{
+  // Create a new Data object with the current date and time
+  var date = new Date();
+  // Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+  // Initalize time to the current time
+  var time = date.toLocaleTimeString();
+  document.getElementById("time").innerHTML = "The current time is " + time + ".";
+  // Source: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+  // Update the displayed time by executing displayTime() every second
+  setInterval(displayTime, 1000);
+}
+
+displayTime();
+
 /*
-We use var instead of const because we want to be able to dynamically update
-the date (time).
+Note that I use var instead of const because I want to be able to dynamically
+update the date (time).
 */
-
-// Create a new Data object with the current date and time
-var date = new Date();
-
-/* Found a better method, scrapped code
-
-// Inititalize variables set to the current time
-// var hours = date.getHours();
-// var minutes = date.getMinutes();
-// var seconds = date.getSeconds();
-*/
-
-// Source: https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
-// Initalize time to the current time
-var time = date.toLocaleTimeString();
-
-document.getElementById("time").innerHTML = "The current time is " + time + ".";
